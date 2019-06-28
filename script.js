642b470a27dbdb4254ddbefcdd949b6762e345e6
Denys Kolesnyk <a.denis.k@gmail.com>
2019-06-28 21:08:53 +0300

    Второй урок

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+let money = 45654;
+let income = 'Freelance';
+let addExpenses = 'Food, Gas, Trip' ;
+let deposit = true;
+let mission = 100000;
+let period = 11;
 
-alert ('Lean JavaScript 8.0');
+console.log(typeof(money));
+console.log(typeof(income));
+console.log(typeof(deposit));
+console.log(income.length);
+console.log('Период ', period ,' месяцев');
+console.log('Цель заработать ', mission ,' долларов США');
+addExpenses = addExpenses.toLocaleLowerCase();
+console.log(addExpenses.split(', '));
 
-console.log('lesson01');
+let budgetDay = money / 30;
+console.log('Дневной бюджет:', budgetDay,'\nОстаток деления:', money % 30);
