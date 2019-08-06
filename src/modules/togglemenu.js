515a922b336a3e3dd63df8a55c9970a8aa5b9b51
Denys Kolesnyk <a.denis.k@gmail.com>
2019-08-06 22:54:18 +0300

    Урок 20

--- src/modules/togglemenu.js
+++ src/modules/togglemenu.js
@@ -1,21 +1,14 @@
-const toggleMenu = () => {
-    const menu = document.querySelector('menu'),
-        menuItem = menu.querySelectorAll('ul>li');
-
-    const handlerMenu = () => {
-        menu.classList.toggle('active-menu');
+    const toggleMenu = () => {
+        const menu = document.querySelector('menu');
+        document.addEventListener('click', (e) => {
+            
+            if (e.target.closest('.menu') && !menu.classList.contains('active-menu')) {
+                menu.classList.add('active-menu');
+            } else if (!(e.target.closest('.active-menu') && !(e.target.closest('.close-btn') 
+                      || e.target.closest('.active-menu a'))) && menu.classList.contains('active-menu')) {
+                menu.classList.remove('active-menu');
+            }
+        });
     };
 
-    document.addEventListener('click', (e) =>{
-        if(e.target.matches('.menu *, menu, .close-btn')){
-            handlerMenu();
-        } else{
-            menu.classList.remove('active-menu');
-        }   
-    });
-
-    
-    menuItem.forEach((e) => e.addEventListener('click', handlerMenu));
-};
-
-    export default toggleMenu;
+export default toggleMenu;
