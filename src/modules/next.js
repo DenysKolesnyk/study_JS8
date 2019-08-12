76b1276b8309db13cec38b98f8d11b1e91d1a597
Denys Kolesnyk <a.denis.k@gmail.com>
2019-08-12 18:47:21 +0300

    Кнопка Следующий шаг

--- src/modules/next.js
+++ src/modules/next.js
@@ -4,31 +4,26 @@ const next = () =>{
         constructBtn  = document.querySelectorAll('.construct-link'),
         constructorAccordion = document.querySelector('#accordion'),
         panelCollapse  = constructorAccordion.querySelectorAll('.panel-collapse');
-        
-        console.log(constructBtn);
 
     constructBtn.forEach((el) =>{
         el.addEventListener('click', (e) =>{
-            
-                console.log(el);
 
-                let target = e.target;
+            let target = e.target;
             
-                target = target.closest('.panel-collapse');
-    
-                target.style.display = 'none';
-                
-                panelCollapse.forEach((item, i) =>{
-                    event.preventDefault();
-                    if(item == target){
-                        panelCollapse[i+1].style.display = 'block';
-                    }
-                });
+            target = target.closest('.panel-collapse');
+
+            target.style.display = 'none';
+            
+            panelCollapse.forEach((item, i) =>{
+                event.preventDefault();
+                if(item == target){
+                    panelCollapse[i+1].style.display = 'block';
+                }
+            });
         });
 
     });
  
 };
-
 
 export default next;
